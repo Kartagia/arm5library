f65e62532b58a90b6194d756a59ba56fc3c5ee93
Antti Kautiainen <kautsu@elisanet.fi>
2024-06-17 10:15:02 +0300

    feat: Basic UI stub for the application
    modified:   src/app/layout.js
    - Replaced the application title and added keywords.
    
    modified:   src/app/page.js
    - Replaced the package initialization content with stub
    Library content.

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { EntryMenuItem, Menu } from "@/components/menu";
+import { menu } from '@/data/dummyMenu.js';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
+        <h1>
+          Ars Magica 5th Edition Campaign tool
+        </h1>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
