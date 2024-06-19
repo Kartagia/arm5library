c700e98e2943815d19cf0f2d9190110d04da0bc4
Antti Kautiainen <kautsu@elisanet.fi>
2024-06-19 11:55:59 +0300

    fix: Library-model invalid identifier type
    modified:   src/data/library.js
    - fix: Invalid ContentModel "id" property type
    to the number.
    - fix: Invalid BookModel "id" property type
    to the number.
    - fix: Invalid CollectionModel "id" property type
    to the number.
    - feat: "Commentary" BookContentType
    - feat: CommentaryBookContentModel
    - feat: TractatusBookContentModel
    - feat: SummaBookContentModel
    - feat: LabtextCotenntModel
    - feat: Optional library, content, and book ids for BookContent.
    
    - feat: Optional library and collection ids for BookModel.
    
    - feat: Optional library id for BookCollectionModel.
    - feat: Optional contents of the BookCollectionModel
    - feat: Optional books of the BookCollectionModel
    
    - feat: Optional saga id for LibraryModel.
    - faet: Optional id for LibraryModel
    - feat: Optional contents of the LibraryModel
    - feat: Optional books of hte LibraryModel
    - feat: Optional collections of hte LibraryModel

--- src/data/library.js
+++ src/data/library.js
@@ -6,7 +6,7 @@
 
 /**
  * The book content types.
- * @typedef {"Tractatus"|"Summa"|"Laboratory Text"} BookContentType
+ * @typedef {"Tractatus"|"Summa"|"Commentary"|"Laboratory Text"} BookContentType
  */
 
 /**
@@ -15,19 +15,127 @@
  */
 
 /**
- * The book content model.
- * @property {tring} id The unique book identifier.
+ * The common properties of the book content model.
+ * @typedef {Object} CommonBookContentModelProps
+ * @property {number} [book_id] The unique book identifier of the
+ * book the content model belongs to.
+ * @property {number} [collection_id] The unique collection identifier of the
+ * collection the content model belongs to.
+ * @property {number} [library_id] The unique library identifier of the
+ * lubrary the content model belongs to.
+ * @property {number} [id] The unique book content identifier. Only content stored
+ * into persisting storage has identifier.
  * @property {string} name The name of the book.
  * @property {BookContentType} [type="Tractatus"] type The book type.
- * @property {string} [title] The title of the book content.
+ * @property {string} [title] The title of the book content. Defaults to the
+ * name.
  * @property {string} [author] THe author of the book content.
  * Defaults to the author of the book.
  */
 
 /**
+ * The properties specific to the laboratory text content.
+ * @typedef {Object} ItemLabtextContentProps 
+ * @property {string|LabtextItemRef} item The item of the lab text.
+ * @property {string} activation The way of activation.
+ * @property {string|LabtextSpellRef} effect The effect
+ * referende. 
+ */
+
+
+/**
+ * A reference to a spell.
+ * @typedef {Object} LabtextSpellRef
+ * @property {RefId} [refId] The referred persisting storage reference identifier
+ * of the referred spell.
+ * @property {string} name The name of the reference.
+ * @property {string} [title] The title of the reference. Defaults to the name.
+ * 
+ */
+
+/**
+ * The properties specific to the laboratory text content.
+ * @typedef {Object} SpellLabtextContentProps
+ * @property {string|LabtextSpellRef} spell The invented spell name or reference. 
+ */
+
+/**
+ * The properties specific to the laboratory text content.
+ * @typedef {SpellLabtextContentProps|ItemLabtextContentProps} LabtextContentProps
+ */
+
+/**
+ * The model of a item enchantment laboratory text content.
+ * @typedef {CommonBookContentModelProps & ItemLabtextContentProps} EnchantmentLabtextContentModel
+ */
+
+
+/**
+ * The model of a spell laboratory text content.
+ * @typedef {CommonBookContentModelProps & SpellLabtextContentProps} SpellLabtextContentModel
+ */
+
+/**
+ * The model of the laboratory texts.
+ * @typedef {EnchantmentLabtextContentModel|SpellLabtextContentModel} LabtextContentModel
+ */
+
+/**
+ * The properties specific to the summa content.
+ * @typedef {Object} SummaContentProps
+ * @property {number} [quality] The quality of the book content.
+ * @property {string} targetType The type of the target.
+ * @property {string} target The target of the tractatus.
+ * @property {number} [capLevel] The cap level of the book content.
+ */
+
+/**
+ * The content model of the summas.
+ * @typedef {CommonBookContentModelProps & SummaContentProps} SummaBookContentModel
+ */
+
+/**
+ * The tractatus content specific properties.
+ * @typedef {Object} TractatusContentProps
+ * @property {string} targetType The type of the target.
+ * @property {string} target The target of the tractatus.
+ * @property {number} quality The quality of the tractatus.
+ */
+
+/**
+ * The properties sepcific to the commentary contents.
+ * @typedef {Object} CommentaryContentProps
+ * @property {number} [source_id] The summa content identifier
+ * of the source summa requried to study this commentary.
+ * @property {string} sourceName The name of the source summa
+ * content the commantyry comments.
+ */
+
+/**
+ * The book content model for Tractatus.
+ * @typedef {CommonBookContentModelProps & TractatusContentProps} TractatusBookContentModel
+ */
+
+/**
+ * The book content model for commentary.
+ * @typedef {TractatusContentProps & CommentaryContentProps} CommentaryBookContentModel
+ */
+
+
+/**
+ * The book content model.
+ * @typedef {TractatusBookContentModel|CommentaryBookContentModel|SummaBookContentModel|LabtextContentModel} BookContentModel
+ */
+
+/**
  * The model of a book.
- * @type {Object} BookModel
- * @property {tring} id The unique book identifier.
+ * @typedef {Object} BookModel
+ * @property {number} [collection_id] The unique collection identifier of the
+ * collection the book model belongs to.
+ * @property {number} [library_id] The unique library identifier of the
+ * lubrary the book model belongs to.
+ * @property {number} [id] The unique book identifier. ONly books
+ * stored in a persisting storage has identifier.
  * @property {string} name The name of the book.
  * @property {BookType} [type="Tractatus"] type The book type.
  * @property {string} [title] The title of the book.
@@ -36,21 +144,33 @@
  */
 
 /**
+ * The model representing a collection of books.
  * @type {Object} BookColectionModel
+ * @property {number} [library_id] The unique library identifier of the library
+ * the library model belongs to.
+ * @property {number} [id] The unique collection identifier. Only
+ * collections stored in a persisting storage has identifier.
  * @property {string} name The name of the collection. The value is
- * unique within the library.
- * @property {(BookModel|string)[]} books THe books of the collection.
+ * unique within the library of the collection.
+ * @property {(BookModel|string)[]} [books=[]] The books of the collection.
  * The string entries are references to the book identifiers of the library containing
  * the book.
+ * @property {(BookContentModel|string)[]} [contents=[]] The book contents of
+ * the collection.
  */
 
 /**
  * The library model interface.
  * @typedef {Object} LibraryModel
- * @property {string} name The library name.
- * @property {BookModel[]} [books] The books of the library.
- * @property {BookCollectionModel[]} [collections] The books of the library.
- * @property {BookContentModel[]} [contents] The books of the library.
+ * @property {number} [saga_id] The unique saga identifier of the sage
+ * the library model belongs to.
+ * @property {number} [id] The unique library identifier. Only libraries
+ * stored in a persisting storage has identifier.
+ * @property {string} name The library name. The name is 
+ * unique withing saga of the library.
+ * @property {BookModel[]} [books=[]] The books of the library.
+ * @property {BookCollectionModel[]} [collections=[]] The books of the library.
+ * @property {BookContentModel[]} [contents=[]] The books of the library.
  */
 
 /**
