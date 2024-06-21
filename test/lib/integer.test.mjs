21f10dfbec3413bb4ba5d2cedf151f437337fb28
Antti Kautiainen <kautsu@elisanet.fi>
2024-06-21 13:18:55 +0300

    fix: test library integer.
    modified:   package.json
    - changed test script to recursive.
    
    modified:   test/lib/integer.test.mjs
    - fix: Invalid tests of checkInteger fixed to test its functionality.
    - fix: Moved truthy cases null, [], and "" into valid cases.

--- test/lib/integer.test.mjs
+++ test/lib/integer.test.mjs
@@ -1,24 +1,25 @@
 
 import { expect } from 'chai';
-import { checkInteger } from '../../src/lib/integers.mjs'
+import { checkInteger } from '../../src/lib/integer.mjs'
 
 describe("checkInteger", function () {
     describe("Valid integers", function () {
-        ["0", 0, 1, "1", 2**8, 2**16,, -(2**8), -(2**16),
+        [null, [], "", "0", 0, 1, "1", 2**8, 2**16,, -(2**8), -(2**16),
             Number.MAX_SAFE_INTEGER, Number.MIN_SAFE_INTEGER
         ].forEach((tested, caseIndex) => {
             it(`Test Case #${caseIndex}: ${String(tested)}`, function () {
-                expect(checkInteger(tested)).false;
+                expect(() => {checkInteger(tested)}).not.throw;
+                expect(checkInteger(tested)).equal(Number(tested));
             })
         });
     });
     describe("Invalid integers", function () {
-        [null, undefined, "", [], {}, 0.5, Number.POSITIVE_INFINITY,
+        [undefined, {}, 0.5, Number.POSITIVE_INFINITY,
         Number.NaN, Number.NEGATIVE_INFINITY,
         Number.MAX_SAFE_INTEGER*2
         ].forEach((tested, caseIndex) => {
             it(`Test Case #${caseIndex}: ${String(tested)}`, function () {
-                expect(checkInteger(tested)).false;
+                expect(() => {checkInteger(tested)}).throw(TypeError);
             })
         });
     });
